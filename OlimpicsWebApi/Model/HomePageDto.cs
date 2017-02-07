31330092fb50dc4222a60a3e04150d882c3ea701
Rogerotti <Rogerottii@gmail.com>
2017-02-08 00:20:10 +0100

    [1] Utworzenie modelu przeznaczonego do strony startowej aplikacji reprezentujący podstawowe dane użytkownika.

---
+++ OlimpicsWebApi/Model/HomePageDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OlimpicsWebApi.Model
+{
+    /// <summary>
+    /// Dto okna startowego aplikacji.
+    /// </summary>
+    public class HomePageDto
+    {
+        public String Username { get; set; }
+
+        public Int32 Level { get; set; }
+
+        public Int32 CurrentExperiance { get; set; }
+
+        public Int32 ExperianceToNextLevel { get; set; }
+
+        public Int32 Coins { get; set; }
+
+        public Int32 Pils { get; set; }
+
+        public Int32 CurrentStamina { get; set; }
+
+        public Int32 StaminaLimit { get; set; }
+    }
+}
